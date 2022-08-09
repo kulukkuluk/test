72f8d95ae8994eec0114829ec71eea8c5e7e9d25
kulukkuluk <110907699+kulukkuluk@users.noreply.github.com>
2022-08-09 18:49:21 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,7 +90,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("CHANNEL BOKEP", url = f"https://t.me/imutgemes"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
@@ -114,7 +114,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "JOIN CHANNEL",
                 url = client.invitelink)
         ]
     ]
@@ -122,7 +122,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = 'COBA LAGI',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
